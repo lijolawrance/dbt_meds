532ea7a1c26a9ac0941c27204db48288e01a8239
lijolawrance <lijolawrance@gmail.com>
2024-02-08 18:18:23 +0530

    updated facility

--- target/compiled/dbt_meds/models/marts/fct_country_sales.sql
+++ target/compiled/dbt_meds/models/marts/fct_country_sales.sql
@@ -1,22 +1,36 @@
-    with source as (
-    SELECT
-        country
-        ,api_sold_product as product_name
-        ,sum(quantity_sold) as total_sold
-        ,round(avg(unit_price), 2) as average_price
-        ,min(sold_product_created_at) as first_sale
-        ,max(sold_product_created_at) as most_recent_sale
-    FROM PUBLIC.RAW_DATA.SALES_DATA
-    WHERE EXTRACT(YEAR FROM sold_product_created_at) = EXTRACT(YEAR FROM current_date) - 1
-    GROUP BY country, api_sold_product
-    HAVING COUNT(*) > 3
-    ORDER BY COUNTRY, api_sold_product
+with sales_data as (
+    select * from public.raw_data.sales_data
+),
+filtered_data as (
+    select 
+        country,
+        api_sold_product 
+    from sales_data
+    where extract(year from sold_product_created_at) = extract(year from current_date) - 1
+    group by 1,2
+    having count(*) >3
+    order by 1
 )
-SELECT 
+final as (
+    select 
+        sales_data.country,
+        sales_data.api_sold_product as product_name,
+        sum(quantity_sold) as total_sold,
+        avg(unit_price) as average_price,
+        min(sold_product_created_at) as first_sale,
+        max(sold_product_created_at) as last_sale
+    from sales_data 
+    inner join filtered_data
+    on filtered_data.country=sales_data.country
+    and filtered_data.api_sold_product=sales_data.api_sold_product
+    group by 1,2
+    order by 1,2
+)
+select 
     country,
     product_name,
     total_sold,
     average_price,
     first_sale,
-    most_recent_sale
-FROM source 
+    last_sale
+from final
